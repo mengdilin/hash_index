90eff668580edcb17afc4ac8ec8287c2c9126bf9
Mengdi Lin <mengdilin95@gmail.com>
2017-02-24 01:40:16 -0500

    update page

---
+++ Page.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "DataEntry.h"
+
+class Page {
+  public:
+    // (total buckets) + overflow page index starting from 0
+    uint64_t overflow_addr;
+    //const static int MAX_ENTRIES = 255;
+    const static int MAX_ENTRIES = 2;
+    uint32_t counter;
+    DataEntry data_entry_list[MAX_ENTRIES];
+    uint32_t hash;
+    //used for debug print bucket record counts
+    bool overflow_merged;
+
+  public:
+    Page();
+    Page(std::vector<DataEntry>);
+    Page(Page&& other);
+    void addEntry(DataEntry);
+    std::ofstream& flush(std::ofstream&);
+    static void read(std::ifstream&, Page&);
+    bool isFull();
+    bool hasOverflow();
+    void setOverflow(uint64_t overflow_addr);
+    void sortEntries();
+    std::pair<bool,uint64_t> find(uint64_t);
+    void mergePage(Page& other);
+};
+
---
+++ plot.py
@@ -0,0 +1,57 @@
+import sys
+import matplotlib.pyplot as plt
+import re
+
+infile = sys.argv[1]
+
+line_start = "primary bucket count: "
+merge_keyword = " merged"
+overflow_keyword = " -> "
+with open(infile) as inf:
+  content = inf.readlines()
+
+merge_count = 0
+entry_distribution = []
+overflow_distribution = []
+pages = 0
+overflows = 0
+empty_page = 0
+max_overflow_chain = 0
+for line in content:
+  line = line.strip('\n')
+  if line.startswith(line_start):
+    #print line
+    line = line[len(line_start):]
+    if merge_keyword in line:
+      count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(merge_keyword), line))
+      merge_count = merge_count + count
+      line = line.replace(merge_keyword, "", 1)
+    if overflow_keyword in line:
+      buckets = line.split(overflow_keyword)
+      buckets = [int(x) for x in buckets]
+      pages = pages + len(buckets)
+      #print "buckets: ", buckets
+      #print "sum: ", sum(buckets)
+      entry_distribution.append(sum(buckets))
+      overflow_distribution.append(sum(buckets[1:]))
+      '''
+      tmp = sum(buckets[1:])
+      if tmp >= 255:
+        print line
+      '''
+      overflows = overflows + len(buckets) - 1
+      max_overflow_chain = max(len(buckets) - 1, max_overflow_chain)
+    else:
+      #print "no overflow: ", int(line)
+      entry_distribution.append(int(line))
+      pages = pages + 1
+print all(isinstance(x, int) for x in entry_distribution)
+print "pages: " , pages
+print "overflows: ", overflows
+print "merge count: ", merge_count
+print "max overflow chain: ", max_overflow_chain
+#plt.hist(entry_distribution, range=[0, 600], alpha=0.5, bins=50, label="key dist")
+#plt.legend(loc='upper right')
+#plt.gca().set_yscale("log")
+#plt.show()
+
