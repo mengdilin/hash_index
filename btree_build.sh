12f6dd1e6512a54371dbc720a54103541c6baca6
Mengdi Lin <mengdilin95@gmail.com>
2017-05-04 21:23:05 -0400

    rename build scripts

--- btree_build.sh
+++ btree_build.sh
@@ -1,10 +1,7 @@
 for i in 128 256 512 1024 2048 4096
-do 
-	echo "Running experiment for btree with page size $i"
-	export BTREE_SIZE=$i
-	sed -i "s/constexpr static unsigned int PAGE_SIZE =.*/constexpr static unsigned int PAGE_SIZE = $i;/g" BTreePage.h
-	cat BTreePage.h | grep "PAGE_SIZE"
-	make
+do
+	echo "nohup ./${i}_test -build /dev/shm/genome/probes/0/data.idx_0_0.500000  /dev/shm/experiments/btree/0/${i}_btree_index > /dev/shm/experiments/btree/0/${i}_btree_log"
+	nohup ./${i}_test -build /dev/shm/genome/probes/0/data.idx_0_0.500000  /dev/shm/experiments/btree/0/${i}_btree_index &> ${i}_btree_log &
 done
 
-echo "=============== done compiling tests ======================"
+echo "===============done running nohup for btree building===================="   
