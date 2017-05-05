21c1c55217e6f833d45c552c61c66e3c91a76fe6
Mengdi Lin <mengdilin95@gmail.com>
2017-05-04 21:22:33 -0400

    rename build files

--- hash_build.sh
+++ hash_build.sh
@@ -1,10 +1,13 @@
 for i in 64 128 256 512 1024 2048 4096
-do 
-	echo "Building indices for experiments for hash with page size $i"
-	export HASH_SIZE=$i
-	sed -i "s/static const int PAGE_SIZE =.*/static const int PAGE_SIZE = $i;/g" Page.h
-	cat Page.h | grep "PAGE_SIZE"
-	make
+do
+	for j in 5 6 7 8 9 
+	do
+		echo "nohup ./${i}_test -build /dev/shm/genome/probes/0/data.idx_0_0.500000  /dev/shm/experiments/hash/0/${i}_${j}0_hash_index 0.${j} &> /dev/shm/experiments/hash/0/${i}_${j}0_hash_log"
+		nohup ./${i}_test -build /dev/shm/genome/probes/0/data.idx_0_0.500000  /dev/shm/experiments/hash/0/${i}_${j}0_hash_index 0.${j} &> /dev/shm/experiments/hash/0/${i}_${j}0_hash_log &
+	done
+
+	nohup ./${i}_test -build /dev/shm/genome/probes/0/data.idx_0_0.500000  /dev/shm/experiments/hash/0/${i}_100_hash_index 1 &> /dev/shm/experiments/hash/0/${i}_100_hash_log &
 done
 
-echo "=============== done compiling tests ======================"
+echo "===============done running nohup for hash building===================="   
+
