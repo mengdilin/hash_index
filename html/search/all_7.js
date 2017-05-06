eb9d4a0c0a99c3e34205461c0eb871341cdd69d7
Mengdi Lin <mengdilin95@gmail.com>
2017-05-06 05:19:20 -0400

    add doxygen

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['page',['Page',['../class_page.html',1,'Page'],['../class_page.html#a9a7cc22d5459498ce638c54ae966c79b',1,'Page::Page()']]],
-  ['parse_5fidx_5ffile',['parse_idx_file',['../class_hash_index.html#af1027d9d3684c93abcc703ccd1824937',1,'HashIndex']]]
+  ['parse_5fidx_5ffile',['parse_idx_file',['../class_hash_index.html#af1027d9d3684c93abcc703ccd1824937',1,'HashIndex']]],
+  ['parse_5fkey_5ffile',['parse_key_file',['../class_hash_index.html#a7b1b1f9d1cd6cec6cae120b338287526',1,'HashIndex']]]
 ];
--- html/search/all_a.js
+++ html/search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['to_20compile',['To compile',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['_7ehashindex',['~HashIndex',['../class_hash_index.html#af8de8c5d009e19dc923f82e5e6cb4ae3',1,'HashIndex']]],
+  ['_7epage',['~Page',['../class_page.html#a2341fff1cc032ab6528874175e7dd841',1,'Page']]]
 ];
